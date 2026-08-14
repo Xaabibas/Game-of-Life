f0804e93bcd2498698cd6c4f024dd5cd97cdd115
Xaabibas <tumuyhabirov@yandex.ru>
2026-08-14 23:39:56 +0300

    help option and improovements

--- game_life.c
+++ game_life.c
@@ -1,9 +1,39 @@
+#include <limits.h>
 #include <ncurses.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+
+const char *help_message = "LAUNCH:\n\r"
+                           "\t./game_life [-h | --help] [-r | --rand <value>] [-d | --delay <value>]\n\r"
+                           "\n\r"
+			   "DESCRIPTION:\n\r"
+			   "\tThis program is a simple implementation of Conway's Game of Life on a torus\n\r"
+			   "\tLearn more: https://en.wikipedia.org/wiki/Conway's_Game_of_Life\n\r"
+			   "\n\r"
+                           "OPTIONS:\n\r"
+                           "\t-h, --help\n\r"
+                           "\t\tPrint reference information about program\n\r"
+			   "\n\r"
+                           "\t-r <value>, --rand <value>\n\r"
+                           "\t\tFill the torus with randomly placed live cells\n\r"
+			   "\t\t<value> specifies the probability of a cell being alive\n\r"
+			   "\t\t<value> must be a non-negative integer no greater than 100\n\r"
+			   "\n\r"
+                           "\t-d <value>, --delay <value>\n\r"
+                           "\t\tSet the delay between generations in milliseconds\n\r"
+			   "\t\t<value> specifies the delay duration\n\r"
+			   "\t\t<value> must be a positive integer\n\r"
+			   "\n\r"
+			   "USAGE:\n\r"
+			   "\tThis program has two modes:\n\r"
+			   "\t  1. Visual mode - run the simulation\n\r"
+			   "\t  2. Edit mode - edit the field and place live cells\n\r"
+			   "\t     Move the cursor with the arrow key or \"h\", \"j\", \"k\", \"l\"\n\r"
+			   "\t     Press Space to toggle a cell\n\r"
+			   "\tPress Enter to switch between modes\n\r"
+			   "\tPress Escape to exit the program\n\r";
 
 enum {
 	success_code = 0,
@@ -50,7 +80,7 @@ void clear_array(int **array, int width, int height)
 	}
 }
 
-void move_cursor(int *x, int *y, int dx, int dy, int max_x, int max_y)
+void update_cursor_position(int *x, int *y, int dx, int dy, int max_x, int max_y)
 {
 	*x += dx + max_x;
 	*x %= max_x;
@@ -60,7 +90,7 @@ void move_cursor(int *x, int *y, int dx, int dy, int max_x, int max_y)
 	move(*y, *x);
 }
 
-void color(int x, int y, int is_alive)
+void draw_cell(int x, int y, int is_alive)
 {
 	move(y, x);
 	if (is_alive) {
@@ -71,11 +101,11 @@ void color(int x, int y, int is_alive)
 	move(y, x);
 }
 
-void invert_field(int **generation, int x, int y)
+void invert_cell(int **generation, int x, int y)
 {
-	int field = generation[x][y];
-	generation[x][y] = field ? 0 :1;
-	color(x, y, field ? 0 : 1);
+	int cell = generation[x][y];
+	generation[x][y] = cell ? 0 : 1;
+	draw_cell(x, y, cell ? 0 : 1);
 }
 
 void increase(int **counter, int x, int y, int width, int height)
@@ -119,7 +149,7 @@ void draw(int **generation, int width, int height)
 	int x, y;
 	for (x = 0; x < width; x++) {
 		for (y = 0; y < height; y++) {
-			color(x, y, generation[x][y]);
+			draw_cell(x, y, generation[x][y]);
 		}
 	}
 }
@@ -129,7 +159,7 @@ void random_fill(int **generation, int width, int height, int prob)
 	int x, y;
 	for (x = 0; x < width; x++) {
 		for (y = 0; y < height; y++) {
-			if (rand() % 100 < prob) {
+			if ((int)(100.0 * rand() / (RAND_MAX + 1.0)) < prob) {
 				generation[x][y] = 1;
 			} else {
 				generation[x][y] = 0;
@@ -138,20 +168,28 @@ void random_fill(int **generation, int width, int height, int prob)
 	}
 }
 
-int string_to_positive_int(char *str) 
+int parse_int(char *str) 
 {
-	int acc = 0, prev = 0;
+	int acc = 0, digit;
+	
+	if (*str == '\0') {
+		return -1;
+	}
 
 	while (*str) {
 		if (*str < '0' || *str > '9') {
 			return -1;
 		}
-		prev = acc;
-		acc *= 10;
-		acc += *str - '0';
-		if (acc < 0 || prev > acc) {
+		digit = *str - '0';
+		
+		if (acc > INT_MAX / 10) {
 			return -1;
 		}
+		acc *= 10;
+		if (acc > INT_MAX - digit) {
+			return -1;
+		}
+		acc += digit;
 		str++;
 	}
 	return acc;
@@ -161,6 +199,7 @@ int main(int argc, char **argv)
 {
 	int height, width, x, y, key, visual_mode = 0;
 	int delay = default_delay_duration;
+	int r_count = 0, d_count = 0;
 	int **generation, **counter;
 	
 	srand(time(NULL));
@@ -168,7 +207,13 @@ int main(int argc, char **argv)
 	initscr();
 	getmaxyx(stdscr, height, width);
 	
-	if (allocate_array(&generation, width, height) || allocate_array(&counter, width, height)) {
+	if (allocate_array(&generation, width, height)) {
+		fprintf(stderr, "Something went wrong\n");
+		endwin();
+		return 1;
+	}
+	if (allocate_array(&counter, width, height)) {
+		free_array(&generation, width);
 		fprintf(stderr, "Something went wrong\n");
 		endwin();
 		return 1;
@@ -178,13 +223,29 @@ int main(int argc, char **argv)
 	argv++;
 	while (*argv) {
 		if (0 == strcmp(*argv, "-h") || 0 == strcmp(*argv, "--help")) {
-			
+			fprintf(stdout, "%s", help_message);
+			endwin();
+			return 0;
 		} else if (0 == strcmp(*argv, "-r") || 0 == strcmp(*argv, "--rand")) {
-			int prob; 
+			int prob;
+			char *option = *argv;
 			argv++;
-			prob = string_to_positive_int(*argv);
+			
+			if (!*argv) {
+				fprintf(stderr, "ERROR - option \"%s\" requires an argument\n\r", option);
+				endwin();
+				return 1;
+			}
+			if (r_count++) {
+				fprintf(stderr, "ERROR - you can't fill randomly twice\n\r");
+				endwin();
+				return 1;
+			}
+
+			prob = parse_int(*argv);
 			if (prob < 0 || prob > 100) {
-				fprintf(stderr, "Probability must be not negative integer less than 100\n");
+				fprintf(stderr, "ERROR - probability must be not negative integer " 
+						"no more than 100: \"%s\"\n\r", *argv);
 				fprintf(stderr, "You can use \"--help\" to get more information\n");
 				endwin();
 				return 1;	
@@ -192,16 +253,33 @@ int main(int argc, char **argv)
 			random_fill(generation, width, height, prob);
 			draw(generation, width, height);
 		} else if (0 == strcmp(*argv, "-d") || 0 == strcmp(*argv, "--delay")) {
+			char *option = *argv;
 			argv++;
-			delay = string_to_positive_int(*argv);
+				
+			if (!*argv) {
+				fprintf(stderr, "ERROR - option \"%s\" requires an argument\n\r", option);
+				endwin();
+				return 1;
+			}
+			if (d_count++) {
+				fprintf(stderr, "ERROR - you can't set delay duration twice\n\r");
+				endwin();
+				return 1;
+			}
+
+			delay = parse_int(*argv);
 			if (delay < 1) {
-				fprintf(stderr, "Delay duration must be positive integer\n");
+				fprintf(stderr, "ERROR - delay duration must be positive "
+						"integer: \"%s\"\n\r", *argv);
 				fprintf(stderr, "You can use \"--help\" to get more information\n");
 				endwin();
 				return 1;
 			}
 		} else {
-		
+			fprintf(stderr, "ERROR - invalid argument: \"%s\"\n\r", *argv);
+			fprintf(stderr, "You can use \"--help\" to get more information\n");
+			endwin();
+			return 1;
 		}
 		argv++;
 	}
@@ -223,7 +301,7 @@ int main(int argc, char **argv)
 		}
 		
 		if (key == '\n' || key == KEY_ENTER) {
-			visual_mode = ~visual_mode + 2;
+			visual_mode = !visual_mode;
 			if (visual_mode) {
 				timeout(0);
 				curs_set(0);
@@ -244,22 +322,22 @@ int main(int argc, char **argv)
 		switch (key) {
 			case KEY_UP:
 			case 'k':
-				move_cursor(&x, &y, 0, -1, width, height);
+				update_cursor_position(&x, &y, 0, -1, width, height);
 				break;
 			case KEY_DOWN:
 			case 'j':
-				move_cursor(&x, &y, 0, 1, width, height);
+				update_cursor_position(&x, &y, 0, 1, width, height);
 				break;
 			case KEY_RIGHT:
 			case 'l':
-				move_cursor(&x, &y, 1, 0, width, height);
+				update_cursor_position(&x, &y, 1, 0, width, height);
 				break;
 			case KEY_LEFT:
 			case 'h':
-				move_cursor(&x, &y, -1, 0, width, height);
+				update_cursor_position(&x, &y, -1, 0, width, height);
 				break;
 			case ' ':
-				invert_field(generation, x, y);
+				invert_cell(generation, x, y);
 		}
 		refresh();
 	}
